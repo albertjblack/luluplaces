4a72139f9df0e379b6916db8905165c322ba2693
albertjblack <albertjblack@outlook.com>
2022-04-06 10:31:12 -0600

    colors, addPlaceScreen and ImageInput

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
+import './add_place_screen.dart';
 
 class PlacesListScreen extends StatelessWidget {
+  static const routeName = "/places";
   const PlacesListScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Your Places"),
-        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.add))],
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
+              },
+              icon: const Icon(Icons.add))
+        ],
       ),
       body: const Center(
         child: CircularProgressIndicator(),
